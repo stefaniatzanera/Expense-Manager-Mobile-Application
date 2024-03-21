0902f7500ed5c942a4470ec4baad3bfa303afb7d
stefaniatzanera <70726781+stefaniatzanera@users.noreply.github.com>
2024-03-21 15:38:21 +0200

    fix to enter in correct page when user has inserted data

--- app/src/main/java/com/example/expensemanagermobileapplication/activities/MainActivity.kt
+++ app/src/main/java/com/example/expensemanagermobileapplication/activities/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.expensemanagermobileapplication.activities
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -8,15 +10,28 @@ import com.example.expensemanagermobileapplication.R
 
 class MainActivity : AppCompatActivity() {
     private val startBtn by lazy { findViewById<Button>(R.id.startButton)}
+    private lateinit var sp: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startBtn.setOnClickListener {
-            //Toast.makeText(this,"pressed", Toast.LENGTH_LONG).show()
-            val intent = Intent(this, BeginningActivity::class.java)
-            startActivity(intent)
-            //onBackPressed()
+        sp = getSharedPreferences("userOptions", MODE_PRIVATE)
+        val isEmpty = isSharedPreferencesEmpty(this, "userOptions")
+        if (isEmpty) {
+            startBtn.setOnClickListener {
+                //Toast.makeText(this,"pressed", Toast.LENGTH_LONG).show()
+                val intent = Intent(this, BeginningActivity::class.java)
+                startActivity(intent)
+                //onBackPressed()
+            }
+        } else {
+            val y = Intent(this, FirstPageActivity::class.java)
+            startActivity(y)
         }
+    }
+
+    fun isSharedPreferencesEmpty(context: Context, sharedPreferencesName: String): Boolean {
+        val sharedPreferences = context.getSharedPreferences("userOptions", Context.MODE_PRIVATE)
+        return sharedPreferences.all.isEmpty()
     }
 }
